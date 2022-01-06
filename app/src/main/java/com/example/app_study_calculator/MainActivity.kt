ae48f8f1fe5d69f9f2ffee25432c446a44defb0e
HyunJin Ko <iamkokoko123@gmail.com>
2022-01-06 18:00:46 +0900

    Second Commit

--- app/src/main/java/com/example/app_study_calculator/MainActivity.kt
+++ app/src/main/java/com/example/app_study_calculator/MainActivity.kt
@@ -8,6 +8,8 @@ import android.text.style.ForegroundColorSpan
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.isVisible
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +19,13 @@ class MainActivity : AppCompatActivity() {
     private val resultTextView: TextView by lazy {
         findViewById<TextView>(R.id.resultTextView)
     }
+    private val historyLayout: View by lazy {
+        findViewById<View>(R.id.historyLayout)
+    }
+    private val historyLinearLayout: View by lazy {
+        findViewById<View>(R.id.historyLinearLayout)
+    }
+
     private var isOperator = false
     private var hasOperator = false
 
@@ -56,7 +65,7 @@ class MainActivity : AppCompatActivity() {
         isOperator = false
 
         val expressionText = expressionTextView.text.split("")
-        if (expressionText.isNotEmpty() || expressionText.last().length >= 15) {
+        if (expressionText.isNotEmpty() && expressionText.last().length >= 15) {
             Toast.makeText(this, "15 자리 까지만 사용할 수 있습니다.", Toast.LENGTH_SHORT).show()
             return
         } else if (expressionText.last().isEmpty() && number == "0") {
@@ -65,7 +74,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         expressionTextView.append(number)
-        // TODO resultTextView 실시간으로 계산 결과를 넣어야 하는 기능
+        resultTextView.text = calculateExpression()
 
     }
 
@@ -87,14 +96,14 @@ class MainActivity : AppCompatActivity() {
                 expressionTextView.append(" $operator")
             }
         }
-
         // 연산자일 경우 초록색으로 표현
         val ssb = SpannableStringBuilder(expressionTextView.text)
         ssb.setSpan(
             ForegroundColorSpan(getColor(R.color.green)),
-            expressionTextView.text.length -1,
+            expressionTextView.text.length - 1,
             expressionTextView.text.length,
-            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
 
         expressionTextView.text = ssb
 
@@ -104,15 +113,87 @@ class MainActivity : AppCompatActivity() {
 
 
     fun resultButtonClicked(v: View) {
+        val expressionTexts = expressionTextView.text.split(" ")
 
+        if (expressionTextView.text.isEmpty() || expressionTexts.size == 1) {
+            return
+        }
+
+        if (expressionTexts.size != 3 && hasOperator) {
+            Toast.makeText(this, "아직 완성되지 않은 수식입니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (expressionTexts[0].isNumber().not() || expressionTexts[2].isNumber().not()) {
+            Toast.makeText(this, "오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val expressionText = expressionTextView.text.toString()
+        val resultText = calculateExpression()
+
+        resultTextView.text = ""
+        expressionTextView.text = resultText
+
+        isOperator = false
+        hasOperator = false
+
+    }
+
+    private fun calculateExpression(): String {
+        val expressionTexts = expressionTextView.text.split(" ")
+
+        if (hasOperator.not() || expressionTexts.size != 3 ) { // 원래 숫자 + 연산자 + 숫자 <- 이렇게 3개가 있어야 함
+            return ""
+        } else if (expressionTexts[0].isNumber().not() || expressionTexts[2].isNumber().not()) {
+            return ""
+        }
+
+        val exp1 = expressionTexts[0].toBigInteger()
+        val exp2 = expressionTexts[2].toBigInteger()
+        val op = expressionTexts[1]
+
+        return when(op) {
+            "+" -> (exp1 + exp2).toString()
+            "-" -> (exp1 - exp2).toString()
+            "*" -> (exp1 * exp2).toString()
+            "/" -> (exp1 / exp2).toString()
+            "%" -> (exp1 % exp2).toString()
+            else -> ""
+        }
     }
 
     fun historyButtonClicked(v: View) {
+        historyLayout.isVisible = true
 
+        // TODO DB에서 모든 기록 가져오기
+        // TODO 뷰에 모든 기록 할당
     }
+
+    fun closeHistoryButtonClicked(v: View) {
+        historyLayout.isVisible = false
+    }
+
+    fun historyClearButtonClicked(v: View) {
+        // TODO DB에서 모든 기록 삭제
+        // TODO 뷰에서 모든 기록 삭제
+    }
+
 
     fun clearButtonClicked(v: View) {
-
+        expressionTextView.text = ""
+        resultTextView.text = ""
+        isOperator = false
+        hasOperator = false
     }
 
+}
+
+fun String.isNumber(): Boolean {
+    return try {
+        this.toBigInteger()
+        return true
+    } catch (e: NumberFormatException) {
+        false
+    }
 }
